4f0cfb692cb538c8fc851a5481357c1d2d74fb8c
RomanovMichael <romanov.players@gmailcom>
2024-02-15 15:55:53 +0300

    refactor(fonts): move connection to the assets

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,9 @@
 </template>
 
 <style lang="scss">
-@font-face {
-    font-family: "Graphik";
-    src: local("GraphikLCG-Regular"), local("GraphikLCG-Regular"),
-        url("~@/assets/fonts/GraphikLCG-Regular.woff2") format("woff2"),
-        url("~@/assets/fonts/GraphikLCG-Regular.woff") format("woff");
-    font-weight: 400;
-    font-style: normal;
-}
-
-@font-face {
-    font-family: "Graphik";
-    src: local("GraphikLCG-Semibold"), local("GraphikLCG-Semibold"),
-        url("~@/assets/fonts/GraphikLCG-Semibold.woff2") format("woff2"),
-        url("~@/assets/fonts/GraphikLCG-Semibold.woff") format("woff");
-    font-weight: 600;
-    font-style: normal;
-}
-
 #app {
     font-family: Graphik;
     font-weight: 600;
+    color: map-get($colors, primary);
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,13 @@ const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
     transpileDependencies: true,
     publicPath: `/zonesmart/`,
+    css: {
+        loaderOptions: {
+            sass: {
+                additionalData: `
+                    @import "@/assets/styles/_fonts.scss";
+                `,
+            },
+        },
+    },
 })
