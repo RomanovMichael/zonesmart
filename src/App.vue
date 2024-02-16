1ebeb9f6d9c76002ae8664823b7a5825d0dd8667
RomanovMichael <romanov.players@gmailcom>
2024-02-16 20:55:59 +0300

    feat: add requests in store

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,89 @@
-<template lang="pug">
-#app
-    RootIcon(name="DeleteSmallIcon")
+<template>
+    <div id="app">
+        <form class="form">
+            <h1 class="form-title">Вход</h1>
+            <div class="form-label">
+                <span>Нет аккаунта?</span>
+                <router-link class="link" to="#"
+                    >Зарегистрироваться</router-link
+                >
+            </div>
+            <div class="form-input">
+                <input
+                    v-model="user_info.email"
+                    type="text"
+                    name="email"
+                    placeholder="yourmail@mail.ru"
+                />
+            </div>
+            <div class="form-input">
+                <input
+                    v-model="user_info.password"
+                    type="text"
+                    name="pasword"
+                    placeholder="Ваш пароль"
+                />
+            </div>
+            <div class="form-label">
+                <router-link class="link" to="#">Забыли пароль?</router-link>
+            </div>
+            <button
+                :disabled="is_btn_disabled"
+                class="form-btn"
+                @click.prevent="logIn"
+            >
+                Войти
+            </button>
+            <button class="form-btn" @click.prevent="getProducts">
+                продукты
+            </button>
+            <div class="">{{ user_info.email }} {{ user_info.password }}</div>
+        </form>
+    </div>
 </template>
 
 <script>
-import RootIcon from "./assets/icons/RootIcon"
-
 export default {
     name: "App",
-    components: { RootIcon },
+    data() {
+        return {
+            user_info: {
+                email: "",
+                password: "",
+            },
+            res: {
+                accessToken: null,
+                refreshToken: null,
+            },
+            products: null,
+        }
+    },
+    computed: {
+        is_btn_disabled() {
+            let btn_disabled =
+                this.user_info.email == "" || this.user_info.password == ""
+            return btn_disabled
+        },
+    },
+    methods: {
+        async logIn() {
+            await this.$store.dispatch("getToken", this.user_info)
+        },
+        async getProducts() {
+            await this.$store.dispatch("getProducts")
+        },
+    },
 }
 </script>
 
 <style lang="scss">
-p {
-    font-size: 1rem;
+.form {
+    padding: 2.5rem 2.5rem 3.75rem;
+    width: 26.875rem;
+    background-color: $white;
+    border-radius: 1rem;
+    &-btn {
+        margin-top: 2.5rem;
+    }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,53 @@
 import Vue from "vue"
 import Vuex from "vuex"
+import axios from "axios"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {},
+    state: {
+        user: {
+            access_token: null,
+            refresh_token: null,
+        },
+        products: null,
+    },
     getters: {},
-    mutations: {},
-    actions: {},
+    mutations: {
+        SET_TOKENS(state, data) {
+            state.user.access_token = data.access
+            state.user.refresh_token = data.refresh
+        },
+        UPDATE_PRODUCTS(state, data) {
+            state.products = data
+        },
+    },
+    actions: {
+        async getToken({ commit }, user_info) {
+            axios
+                .post("https://dev-ar.zonesmart.com/api/user/jwt/create/", {
+                    ...user_info,
+                })
+                .then((response) => {
+                    commit("SET_TOKENS", response.data)
+                })
+                .catch((err) => console.log(err))
+        },
+        async getProducts({ commit, state }) {
+            const params = new URLSearchParams()
+            params.append("limit", 10)
+            params.append("offset", 4)
+
+            axios
+                .get(`https://dev-ar.zonesmart.com/api/product/?${params}`, {
+                    headers: {
+                        Authorization: `JWT ${state.user.access_token}`,
+                    },
+                })
+                .then((response) => console.log(response))
+                .then((response) => commit("UPDATE_PRODUCTS", response))
+                .catch((err) => console.log(err))
+        },
+    },
     modules: {},
 })
