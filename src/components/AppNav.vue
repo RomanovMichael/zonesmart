e28942ea95c01cd77b3184622fb0e64cb8d821e9
RomanovMichael <romanov.players@gmailcom>
2024-02-20 07:58:27 +0300

    chore(nav): add hover effects and rewrite on pug

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,16 +1,10 @@
-<template>
-    <nav class="app-nav flex --align-center --just-end">
-        <button class="app-nav-control">
-            <RootIcon name="BellIcon" />
-        </button>
-        <button
-            v-if="$store.state.user_mail"
-            class="app-nav-control flex --align-center"
-        >
-            {{ $store.state.user_mail }}
-            <RootIcon name="ArrowIcon" />
-        </button>
-    </nav>
+<template lang="pug">
+nav.app-nav
+    button.app-nav-control.app-nav-control--notif
+        root-icon(name="BellIcon")
+    button.app-nav-control.app-nav-control--user(v-if="$store.state.user_mail")
+        | {{ $store.state.user_mail }}
+        root-icon(name="ArrowIcon")
 </template>
 
 <script>
@@ -26,6 +20,9 @@ export default {
 <style lang="scss">
 .app-nav {
     position: fixed;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
     top: 0;
     left: 0;
     right: 0;
@@ -34,5 +31,28 @@ export default {
     padding: 1.75rem 1.875rem;
     gap: 1.875rem;
     z-index: 1;
+
+    &-control {
+        display: flex;
+        align-items: center;
+
+        &.app-nav-control--notif {
+            color: $gray;
+            transition: color 0.3s ease;
+            &:hover {
+                color: $black;
+                transition: color 0.3s ease;
+            }
+        }
+
+        &.app-nav-control--user {
+            color: $black;
+            transition: color 0.3s ease;
+            &:hover {
+                color: $green;
+                transition: color 0.3s ease;
+            }
+        }
+    }
 }
 </style>
